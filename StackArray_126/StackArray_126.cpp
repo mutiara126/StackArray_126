68e01539e5398fd067b2a3702a0a664c2fb8a2f4
mutiara126 <salsamutiara1812@gmail.com>
2023-06-15 16:23:32 +0800

    untuk menampilkan step 2 void pop

--- StackArray_126/StackArray_126.cpp
+++ StackArray_126/StackArray_126.cpp
@@ -16,25 +16,25 @@ public:
 			cout << "Number of data exceeds the limit," << endl;
 			return "";
 		}
+		
 		top++;  //step 2
 		stack_array[top] = element;  //step 3
 		cout << endl;
 		cout << element << "ditambahkan(pushed)" << endl;
 
+		
 		return element;
 	}
 
-
-
-	void pop() {
+     void pop() {
 		if (empty()) { //step 1
-			cout << "\nStack is empty. Cannot pop." << endl;  
+			cout << "\nStack is empty. Cannot pop." << endl;
 			return; //1.b
 
+		}
 
-		
-
-	}
+		cout << "\nThe popped element is: " << stack_array[top] << endl; //step 2
+	
 };
 
 
