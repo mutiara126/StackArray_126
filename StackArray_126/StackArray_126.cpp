ebe0e58da85d89f4f2b12d79a75b6ed1244f4432
mutiara126 <salsamutiara1812@gmail.com>
2023-06-15 16:04:45 +0800

    untuk menambahkan element

--- StackArray_126/StackArray_126.cpp
+++ StackArray_126/StackArray_126.cpp
@@ -11,6 +11,15 @@ public:
 	StackArray() {
 		top = -1;
 	}
+	string push(string element) {
+		if (top == 4) { //step 1
+			cout << "Number of data exceeds the limit," << endl;
+			return "";
+		}
+	}
+};
+
+
 
 
 
